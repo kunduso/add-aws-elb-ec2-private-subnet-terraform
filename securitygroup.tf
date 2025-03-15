bfc9aa81be313ac44995a56582af007d17eaaa7d
Sourav Kundu <skundu.dev@gmail.com>
2025-03-15 11:54:57 -0500

    updated security groups

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,38 +1,59 @@
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.this.id
-}
-resource "aws_security_group" "ec2_instance" {
-  name        = "App-2-IN-SG"
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "ec2_security_group" {
+  name        = "${var.name}-Instance-SG"
   description = "Allow inbound and outbound traffic to EC2 instances from load balancer security group"
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lb.id]
+  vpc_id      = module.vpc.vpc.id
+  tags = {
+    "Name" = "${var.name}-instance-sg"
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  vpc_id = aws_vpc.this.id
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "ingress_ec2" {
+  description              = "allow traffic from the load balancer"
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.lb_security_group.id
+  security_group_id        = aws_security_group.ec2_security_group.id
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "egress_ec2" {
+  description       = "allow traffic to the load balancer"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ec2_security_group.id
 }
 
-resource "aws_security_group" "lb" {
-  name        = "App-2-LB-SG"
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "lb_security_group" {
   description = "Allow inbound and outbound traffic to load balancer from the internet."
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  name        = "${var.name}-LB-SG-IN"
+  vpc_id      = module.vpc.vpc.id
+  tags = {
+    "Name" = "${var.name}-lb-sg"
   }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  vpc_id = aws_vpc.this.id
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "ingress_load_balancer" {
+  description       = "Allow traffic into the load balancer from the internet."
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lb_security_group.id
+}
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule
+resource "aws_security_group_rule" "egress_load_balancer" {
+  description       = "Allow traffic to reach outside the vpc."
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lb_security_group.id
 }
