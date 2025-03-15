359839ca8ae2aca7423594662255bd79e955bc7f
Sourav Kundu <skundu.dev@gmail.com>
2025-03-15 16:31:02 -0500

    more fixes

--- compute.tf
+++ compute.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "app-server" {
   count                  = length(var.subnet_cidr_private)
   instance_type          = var.instance_type
   ami                    = data.aws_ami.amazon_ami.id
-  vpc_security_group_ids = [aws_security_group.ec2_instance.id]
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   subnet_id              = element(module.vpc.private_subnets.*.id, count.index)
   metadata_options {
     http_endpoint               = "enabled"
