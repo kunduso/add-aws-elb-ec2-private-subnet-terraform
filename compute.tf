a163c202622a40d1a162b13a473d06ef85e6b0c1
Sourav Kundu <skundu.dev@gmail.com>
2025-03-19 23:05:11 -0500

    fix for #39, #40, #41, #42, #43

--- compute.tf
+++ compute.tf
@@ -1,4 +1,4 @@
-
+#https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
 data "aws_ami" "amazon_ami" {
   filter {
     name   = "name"
@@ -15,10 +15,23 @@ resource "aws_instance" "app-server" {
   count                  = length(var.subnet_cidr_private)
   instance_type          = var.instance_type
   ami                    = data.aws_ami.amazon_ami.id
-  vpc_security_group_ids = [aws_security_group.ec2_instance.id]
-  subnet_id              = element(aws_subnet.private.*.id, count.index)
-  tags = {
-    Name = "app-2-server-${count.index + 1}"
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
+  subnet_id              = element(module.vpc.private_subnets.*.id, count.index)
+  ebs_optimized          = true
+  monitoring             = true
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+    instance_metadata_tags      = "enabled"
   }
-  user_data = file("user_data/user_data.tpl")
+  root_block_device {
+    encrypted = true
+  }
+  tags = {
+    Name = "${var.name}-server-${count.index + 1}"
+  }
+  user_data     = filebase64("./user_data/user_data.tpl")
+  #checkov:skip=CKV2_AWS_41: Ensure an IAM role is attached to EC2 instance
+  #This EC2 instance does not interact with any AWS service
 }
