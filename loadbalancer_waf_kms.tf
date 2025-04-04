19359889190fd747375c111bb64d83080dab4e0d
Sourav Kundu <skundu.dev@gmail.com>
2025-04-03 22:27:56 -0500

    corrected waf log group names

--- loadbalancer_waf_kms.tf
+++ loadbalancer_waf_kms.tf
@@ -1,6 +1,6 @@
 data "aws_region" "current" {}
 locals {
-  waf_log           = "/aws/waf/${var.name}-waf-logs"
+  waf_log           = "aws-waf-logs-${var.name}"
   principal_root_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
   principal_logs_arn = "logs.${data.aws_region.current.name}.amazonaws.com"
   waf_log_group_arn = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${local.waf_log}"
