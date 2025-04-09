593ca45de909fba92fe83ffa906bdef129d3d53c
Sourav Kundu <skundu.dev@gmail.com>
2025-04-09 13:43:50 -0500

    update comment

--- provider.tf
+++ provider.tf
@@ -17,7 +17,7 @@ provider "aws" {
     }
   }
 }
-# Provider for us-east-1 (required for DNSSEC KMS key)
+# Provider for us-east-1 (required for DNS query logging)
 provider "aws" {
   alias      = "us-east-1"
   region     = "us-east-1"
