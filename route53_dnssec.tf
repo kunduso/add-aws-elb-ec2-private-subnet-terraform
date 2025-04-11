82666500869308a85db7b8bde18db9bbff1f4bd9
Sourav Kundu <skundu.dev@gmail.com>
2025-04-11 15:22:37 -0500

    added dependency

--- route53_dnssec.tf
+++ route53_dnssec.tf
@@ -65,6 +65,7 @@ resource "aws_route53_key_signing_key" "main" {
   key_management_service_arn = aws_kms_key.dnssec.arn
   name                       = "${var.name}-key"
   status                     = "ACTIVE"
+  depends_on = [ aws_kms_key.dnssec ]
 }
 resource "aws_route53_hosted_zone_dnssec" "dns" {
   depends_on = [
