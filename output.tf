7ee8977cef750dee5ea293dc661c748a7a84672a
Sourav Kundu <skundu.dev@gmail.com>
2025-04-13 21:40:44 -0500

    update output for DS record

--- output.tf
+++ output.tf
@@ -1,3 +1,14 @@
 output "route53_name_servers" {
   value = aws_route53_zone.main.name_servers
 }
+
+# Output DS record
+output "ds_record" {
+  description = "Formatted DS record for registrar"
+  value = format("%s %s %s %s",
+    aws_route53_key_signing_key.main.key_tag,
+    aws_route53_key_signing_key.main.signing_algorithm_mnemonic,
+    aws_route53_key_signing_key.main.digest_algorithm_mnemonic,
+    aws_route53_key_signing_key.main.digest_value
+  )
+}
